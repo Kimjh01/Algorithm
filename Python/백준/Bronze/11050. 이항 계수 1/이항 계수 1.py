c95d975be6d844d40da067719cb0f8df01ea8887
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-08-26 00:53:20 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ Python/백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,3 +1,7 @@
-from math import comb
-N, M= map(int,input().split())
-print(comb(N, M))
+n, k = map(int, input().split())
+k = min(k, n-k)  
+num = und = 1
+for i in range(1, k+1):
+    num *= n - (i - 1)
+    und *= i
+print(num // und)
