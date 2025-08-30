4c5332e89e3f2c92eda6ba37a2d079f86dd3523b
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-08-30 20:03:32 +0900

    Delete PyPy3 directory

--- PyPy3/백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++
@@ -1,4 +0,0 @@
-N = int(input())
-nums = input().strip()   
-total = sum(int(c) for c in nums)
-print(total)
--- PyPy3/백준/Silver/1026. 보물/보물.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-
-A.sort()
-result = sum(a * b for a, b in zip(A, sorted(B, reverse=True)))
-print(result)
