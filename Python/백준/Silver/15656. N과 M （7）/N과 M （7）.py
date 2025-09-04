74e2a255d4428031c8f89bb79958b928608b4db1
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-09-04 16:44:28 +0900

    [Silver III] Title: N과 M (7), Time: 1984 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ Python/백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,10 +1,14 @@
-from itertools import product
-
 N, M = map(int, input().split())
-
 arr = sorted(list(map(int, input().split())))
+result = []
 
-result = list(product(arr, repeat=M))
-
-for row in result:
-    print(*row)
+def dfs():
+    if len(result) == M:
+        print(*result)
+        return
+    for x in range(N):
+        result.append(arr[x])
+        dfs()  
+        result.pop()
+        
+dfs()
