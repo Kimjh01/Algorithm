b8f358291f557f5b2ea8a229f085e35091dd2b8f
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-09-04 16:58:41 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ Python/백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,10 +1,14 @@
-from itertools import combinations_with_replacement
-
 N, M = map(int, input().split())
+arr = sorted(set(map(int, input().split())))
+result = []
 
-arr = sorted(list(map(int, input().split())))
-
-result = sorted(list(set(combinations_with_replacement(arr, M))))
-
-for row in result:
-    print(*row)
+def dfs(x):
+    if len(result) == M:
+        print(*result)
+        return
+    for y in range(x, len(arr)):
+        result.append(arr[y])
+        dfs(y)  
+        result.pop()
+        
+dfs(0)
