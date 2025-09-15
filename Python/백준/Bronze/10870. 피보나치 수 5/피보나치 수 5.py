aa985fe3906364b7eedd897b48c947df597a86f8
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-09-16 08:47:35 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ Python/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,10 @@
-def fibo(n):
-    if n <= 0:
-        return 0
-    if n == 1 or n == 2:
-        return 1
-    dp = [0] * (n+1)
-    dp[1], dp[2] = 1, 1
-    for i in range(3, n+1):
-        dp[i] = dp[i-1] + dp[i-2]
-    return dp[n]
+def fibo(n):	
+	if n == 2 or n == 1:
+		return 1
+	elif n <= 0:
+		return 0
+	else:
+		return fibo(n-1) + fibo(n-2)
 
 N = int(input())
-print(fibo(N))
+print(fibo(N)) 
