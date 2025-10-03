8aa4fab8fed640f21d049ff0f8b9be87064d1113
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-04 00:35:11 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string line;
+    long long cnt = 0;
+    while (getline(cin, line)) 
+        ++cnt;
+    cout << cnt << '\n';
+    return 0;
+}
