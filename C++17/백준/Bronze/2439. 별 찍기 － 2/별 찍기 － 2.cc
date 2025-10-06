d5a0d97d455383d8c7ec6cbd8b6ffabec1f812ca
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-06 23:16:39 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N; 
+    if (!(cin >> N)) return 0;
+    for (int i = 1; i <= N; ++i) {
+        cout << string(N - i, ' ') << string(i, '*') << '\n';
+    }
+    return 0;
+}
