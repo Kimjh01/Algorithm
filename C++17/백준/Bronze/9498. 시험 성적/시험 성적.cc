553fa49172f44c6b3bbe3200d2e139e59248bc7a
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-09 19:24:50 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N < 60){
+      cout << 'F';
+    }
+    else if (N < 70)
+    {
+      cout << 'D';
+    }
+    else if (N < 80)
+    {
+      cout << 'C';
+    }
+    else if (N < 90)
+    {
+      cout << 'B';
+    }
+    else
+      cout << 'A';
+    return 0;
+}
