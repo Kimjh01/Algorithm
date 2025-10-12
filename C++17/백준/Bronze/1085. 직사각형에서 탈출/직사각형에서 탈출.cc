c471bffde604400ec4ecd8600d3e62bbe8fe666e
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-12 15:21:45 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x, y, w, h; 
+    if (!(cin >> x >> y >> w >> h)) 
+        return 0;
+    int ans = min(min(x, y), min(w - x, h - y));
+    cout << ans << '\n';
+    return 0;
+}
