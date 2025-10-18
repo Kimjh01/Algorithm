a4c0a9f2214a9aa132979a46e1fad6f22406dc9b
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-18 15:23:02 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    long long n, s;
+    while (cin >> n >> s) cout << s / (n + 1) << '\n';
+    return 0;
+}
