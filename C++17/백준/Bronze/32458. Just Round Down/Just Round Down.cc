829e6d69ce5c181ee5d5e3b61d7d1efca460036c
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-26 08:22:23 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    long double x; if(!(cin >> x)) return 0;
+    long long ans = (long long)x;  // toward zero
+    cout << ans;
+}
