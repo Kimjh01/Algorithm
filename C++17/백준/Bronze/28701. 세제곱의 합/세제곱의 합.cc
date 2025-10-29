df4dd966291ae0d698aebd2077ab4b9eda20f560
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-29 09:16:08 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+	long long N;
+   	cin >> N;
+    cout << N*(N+1)/2<< "\n";
+    cout << (N*(N+1)/2)*(N*(N+1)/2) << "\n";
+    cout << (N*(N+1)/2)*(N*(N+1)/2);
+    return 0;    
+}
