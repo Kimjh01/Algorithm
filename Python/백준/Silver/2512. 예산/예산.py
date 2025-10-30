9bd56cb1fcb498a147085c51de9cea4ba20340a3
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-10-30 09:23:01 +0900

    [Silver II] Title: 예산, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Silver/2512. 예산/예산.py
+++ Python/백준/Silver/2512. 예산/예산.py
@@ -1,25 +1,38 @@
 import sys
-
 input = sys.stdin.readline
+
+# 지방의 수
 n = int(input())
+
+# 각 지방의 예산요청 
 requests = list(map(int, input().split()))
+
+# 총 예산 
 total = int(input())
 
+# 만일 지방 예산 요청 총합이 총 예산보다 작으면 바로 나눠주고 치워버려
 if sum(requests) <= total:
     print(max(requests))
     sys.exit(0)
 
-lo, hi = 0, max(requests)
+# 이진탐새을 통해 적절한 값 찾기 
+low, high = 0, max(requests)
 ans = 0
-while lo <= hi:
-    mid = (lo + hi) // 2  
+while low <= high:
+
+    # 각 지방의 예산 요청에 평균을 기준
+    mid = (low + high) // 2  
     s = 0
+    
+    # 예산들을 하나씩 비교
     for r in requests:
         s += r if r <= mid else mid
+    
+    # 평균 또는 예산들의 합이 같지 않으면 범위 조정
     if s <= total:
         ans = mid     
-        lo = mid + 1
+        low = mid + 1
     else:
-        hi = mid - 1
+        high = mid - 1
 
 print(ans)
