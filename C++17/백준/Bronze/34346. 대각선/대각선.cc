1c02055db597226845c6e88e16004d1a8a1877b6
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-11-13 08:33:55 +0900

    [Bronze IV] Title: 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34346. 대각선/대각선.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long N;
+    cin >> N;
+    if (N % 2 == 1) {
+        cout << 1;
+    } else {
+        cout << 2;
+    }
+    return 0;
+}
