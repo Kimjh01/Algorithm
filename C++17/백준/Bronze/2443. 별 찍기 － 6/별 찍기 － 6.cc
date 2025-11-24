9a15de7b4ef3efc809076acef72ed76271f9942e
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2025-11-25 08:56:52 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int spaces = i;
+        int stars  = 2 * (N - i) - 1;
+
+        for (int j = 0; j < spaces; j++) 
+            cout << ' ';
+        for (int j = 0; j < stars; j++)  
+            cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
