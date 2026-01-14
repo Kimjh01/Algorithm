c078c52d318fd00c2b391c53c69ae5e6769e28a2
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2026-01-14 23:17:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
+++ C++17/백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -2,12 +2,18 @@
 
 using namespace std;
 
-int main(){
-	int x, y;
+int main() {
+    int x, y;
     cin >> x >> y;
-    if(x > 0 && y > 0) cout << "1";
-    else if(x < 0 && y > 0) cout << "2";
-    else if(x < 0 && y < 0) cout << "3";
-    else if(x > 0 && y < 0) cout << "4";
+
+    if (x > 0 && y > 0) 
+        cout << 1;
+    else if (x < 0 && y > 0) 
+        cout << 2;
+    else if (x < 0 && y < 0) 
+        cout << 3;
+    else 
+        cout << 4;
+
     return 0;
 }
