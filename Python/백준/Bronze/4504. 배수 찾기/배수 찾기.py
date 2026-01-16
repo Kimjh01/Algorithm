c15e8264f8b428c254cc55591b3782531a1eeb04
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2026-01-16 23:58:47 +0900

    [Bronze III] Title: 배수 찾기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+while True:
+    t = int(input())
+    if t == 0:
+        break
+    
+    if t % n == 0:
+        print(f"{t} is a multiple of {n}.")
+    else:
+        print(f"{t} is NOT a multiple of {n}.")
