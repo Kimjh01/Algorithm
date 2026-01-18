d4a75f14465feaeb89352a76ae5925099a73e2da
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2026-01-19 08:37:18 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+    while (n > 0) {
+        if (n % 5 == 0) {
+            count += n / 5;
+            n = 0;
+            break;
+        }
+        n -= 2;
+        count++;
+    }
+
+    if (n < 0) cout << -1;
+    else cout << count;
+
+    return 0;
+}
