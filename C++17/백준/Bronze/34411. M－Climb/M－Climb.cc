8f29d080a9c8abb4934c8711388f27b50b9a947d
Kimjuho <144080823+Kimjh01@users.noreply.github.com>
2026-01-21 23:29:10 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long n, m, year;
+    if (!(cin >> n)) 
+        return 0;
+    cin >> m >> year;
+    cout << n * m;
+    return 0;
+}
